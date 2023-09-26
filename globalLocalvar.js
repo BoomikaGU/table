73cfc90b7c33dbf3f092b780f6209907e8c4f381
BoomikaGU <132331855+BoomikaGU@users.noreply.github.com>
2023-09-26 11:37:18 +0530

    Add files via upload

---
+++ globalLocalvar.js
@@ -0,0 +1,13 @@
+var fullName;
+function greet(){
+    fullName = "siddhartha";
+console.log(fullName);
+}
+
+    function greet2(){
+        fullName = "siddhartha kumar";
+    console.log(fullName);
+}
+greet();
+greet2();
+greet();
