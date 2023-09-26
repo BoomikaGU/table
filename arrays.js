ff2d758b2ce9c468609f2409ce7e9eaf8320fdc4
BoomikaGU <132331855+BoomikaGU@users.noreply.github.com>
2023-09-26 14:11:05 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,16 @@
+// const carBrand = ["BMW", "Audi", "Jaguar"];
+// carBrand.forEach((item,index)=> {
+//     if (item === 'Jaguar'){
+//         carBrands[index] = 'Honda';
+//     }
+// })
+// console.log(carBrands);
+
+
+const Movies = new Array(5);
+movie[0] ="";
+movie[1] ="";
+movie[2] ="";
+movie[3] ="";
+movie[4] ="";
+console.log(movies);
