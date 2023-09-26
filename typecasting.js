7b911c810d39c68a5aaa7c0b8dfa791436680fe4
BoomikaGU <132331855+BoomikaGU@users.noreply.github.com>
2023-09-26 10:34:03 +0530

    Add files via upload

---
+++ typecasting.js
@@ -0,0 +1,7 @@
+var contanct = "9898989898";
+var covertedContect = +contanct;
+var rollNumber = "2344AHDB";
+console.log(+rollNUmber);
+var isLoggedIn = false;
+var convertedIsLoggedIn = isLoggedIn +"";
+console.log(typeof convertedIsLoggedIn);
